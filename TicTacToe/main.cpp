e7b08cb83e89508de7751c4762f63231ca6da14e
Quoc141119 <cubi12c6@gmail.com>
2019-06-07 10:04:11 +0700

    them 2 function : player1play and player2play

--- TicTacToe/main.cpp
+++ TicTacToe/main.cpp
@@ -1,68 +1,79 @@
 #include <cstdlib>
 #include <iostream> 
+#include <iomanip>
+#include <math.h>
 using namespace std;
 
 
 // change do kho cua game
-const int maxarray = 10;
-int sizearray = 100; 
+const int maxarray = 5;
+int sizearray = pow(maxarray,2); 
+char array[maxarray][maxarray];
+char player1[20], player2[20];
 
+//comfirm to play 
 int welcome(){
-	int c;
+	int mode;
 	cout<<"Welcome To Tic-tac-toe game! Play with your way!\n";
 	cout<<"If you find any problem, please contact john.nguyen@gameloft.com\n";
 	cout<<"\nSELECT YOUR MODE (1 - PLAY GAME, OTHERS - EXIT GAME): ";
-	cin>>c;
-	return c;
+	cin>>mode;
+	return mode;
 }
 
-void showarray(char a[maxarray][maxarray]){
+//show array
+void showarray(char *array){
 	for(int i=0;i<maxarray;i++){
 		for(int j=0;j<maxarray;j++){
-			if(a[i][j]!='X' && a[i][j]!='O')
-				cout<<"-"<<"  ";
+			if(*array!='X' && *array!='O')	
+				cout<<setw(4)<<"-";
 			else
-				cout<<a[i][j]<<"  ";
-		}		
-		cout<<endl;
+				cout<<setw(4)<<*array;
+			array++;
+		}
+		cout<<endl<<endl;
 	}
 }
 
-//check vi tri danh co hop le khong
-int check_dd(char a[maxarray][maxarray], int b){
-	if(b/10>(maxarray-1) || b/10<0 || b%10>(maxarray-1)){
+//check point
+int checkpoint(char array[maxarray][maxarray], int point){
+	
+	if(point/10>(maxarray-1) || point<0 || point%10>(maxarray-1)){
 		cout<<"khong xac dinh duoc vi tri\n";
 		return 0;
 	}
-	if(a[b/10][b%10]=='X' || a[b/10][b%10]=='O'){
+	
+	if(array[point/10][point%10]=='X' || array[point/10][point%10]=='O'){
 		cout<<"Vi tri da duoc chon\n";
 		return 0;
 	}
+	
 	return 1;
 }
 
-int checkwin(char a[maxarray][maxarray], int b, int c){
-	char x;
+//check winer 
+int checkwin(char array[maxarray][maxarray], int point, int idsign){
+	char sign;
 	int check = 1;
 	
-	if(c%2==0)
-		x ='O';
+	if(idsign%2==0)
+		sign ='O';
 	else
-		x ='X';	
+		sign ='X';	
 	
-	//check ngang	
+	//check row	
 	for(int j = 0; j<maxarray; j++){
-		if(a[b/10][j]!=x){
+		if(array[point/10][j]!=sign){
 			check = 0;
 			break;
 		}			
 	}
 	
-	//check doc
+	//check column
 	if(check == 0){
 		check = 1;
 		for(int j = 0; j<maxarray; j++){
-			if(a[j][b%10]!=x){
+			if(array[j][point%10]!=sign){
 				check = 0;
 				break;
 			}			
@@ -71,10 +82,10 @@ int checkwin(char a[maxarray][maxarray], int b, int c){
 	
 	//check cheo
 	if(check == 0){
-		if(b%10==b/10){	
+		if(point%10==point/10){	
 			check = 1;
 			for(int j=0; j<maxarray; j++){
-				if(a[j][j]!=x){
+				if(array[j][j]!=sign){
 					check = 0;
 					break;
 				}			
@@ -82,11 +93,12 @@ int checkwin(char a[maxarray][maxarray], int b, int c){
 		}
 	}
 	
+	//check cheo
 	if(check == 0){
-		if((b%10)+(b/10)==maxarray-1){
+		if((point%10)+(point/10)==maxarray-1){
 			check = 1;
 			for(int j=0; j<maxarray; j++){
-				if(a[j][maxarray-1-j]!=x){
+				if(array[j][maxarray-1-j]!=sign){
 					check = 0;
 					break;
 				}			
@@ -97,55 +109,84 @@ int checkwin(char a[maxarray][maxarray], int b, int c){
 	return check;
 }
 
-int main(int argc, char** argv) {
-	char a[maxarray][maxarray];
-	char player1[20], player2[20];
+//player1 play
+int player1play(int idsign){
 	int win = 0;
+	int point;
 	
-	if(welcome()==1){
-		cout<<"Enter Player 1 Name: ";
-		cin>>player1;
-		cout<<"Enter Player 2 Name: ";
-		cin>>player2;
-		for(int i=0; i<sizearray; i++){
-			system("cls");
-			showarray(a);
-			int arr;
-			if(i%2==0){
-				do{
-					cout<<"Your Turn "<<player1<<" - O: ";
-					cin>>arr;
-				}while(check_dd(a,arr)==0);
-				a[arr/10][arr%10]='O';
-				system("cls");
-				showarray(a);
-				if(checkwin(a,arr,i) == 1){
-					cout<<player1<<" WIN\n";
-					cout<<"CHUC MUNG NHE !!!";
-					win = 1;
-					break;
-				}
-			}
-			else{
-				do{
-					cout<<"Your Turn "<<player2<<" - X: ";
-					cin>>arr;
-				}while(check_dd(a,arr)==0);
-				a[arr/10][arr%10]='X';
-				system("cls");	
-				showarray(a);
-				if(checkwin(a,arr,i) == 1){
-					cout<<player2<<" WIN\n";
-					cout<<"CHUC MUNG NHE !!!";
-					win = 1;
-					break;
-				}
-			}
-		}
+	do{
+		cout<<"Your Turn "<<player1<<" - O: ";
+		cin>>point;
+	}while(checkpoint(array,point)==0);
+	
+	array[point/10][point%10]='O';
+	system("cls");
+	showarray(*array);
+	
+	if(checkwin(array,point,idsign) == 1){
+		cout<<player1<<" WIN\n";
+		cout<<"CHUC MUNG NHE !!!\n";
+		win = 1;
 	}
 	
-	if(win==0)
-		cout<<"HOA ROI NHE !!!";
+	return win;
+}
+
+//player2 play
+int player2play(int idsign){
+	int win = 0;
+	int point;
+	
+	do{
+		cout<<"Your Turn "<<player2<<" - X: ";
+		cin>>point;
+	}while(checkpoint(array,point)==0);
+	
+	array[point/10][point%10]='X';
+	system("cls");	
+	showarray(*array);
+
+	if(checkwin(array,point,idsign) == 1){
+		cout<<player2<<" WIN\n";
+		cout<<"CHUC MUNG NHE !!!\n";
+		win = 1;
+	}
+	
+	return win;
+}
+
+//start play
+void play(){
+	int win;
+	
+	cout<<"Enter Player 1 Name: ";
+	cin>>player1;
+	cout<<"Enter Player 2 Name: ";
+	cin>>player2;
+	
+	//so chan thi player1 play and so le thi player2 play
+	for(int i=0; i<sizearray; i++){
+		system("cls");
+		showarray(*array);
 		
+		if(i%2==0){    	//player1 play
+			win = player1play(i);
+			if(win == 1)
+				break;
+		}
+		else{			//player2 play
+			win = player2play(i);
+			if(win == 1)
+				break;
+		}
+	}
+
+	if(win==0)
+		cout<<"HOA ROI NHE !!!\n";
+}
+
+int main(int argc, char** argv) {
+	if(welcome()==1)
+		play();			
 	return 0;
 }
