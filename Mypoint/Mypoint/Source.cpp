98a0c70ad0376e056b6c86b190172f36b4e2af57
Quoc141119 <cubi12c6@gmail.com>
2019-06-12 16:57:28 +0700

    + Add project mypoint include some function constructor, destructor, set, get, display, distance (return distance of two point)

---
+++ Mypoint/Mypoint/Source.cpp
@@ -0,0 +1,14 @@
+#include "Mypoint.h"
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	MyPoint *p1 = new MyPoint(1,5);
+	MyPoint *p2 = new MyPoint(6,2);
+	p1->Display();
+	cout << p1->Distance(*p2) << endl;
+	system("pause");
+	return 0;
+}
