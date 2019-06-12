c0c7cce6636763db296411d1c3a87adfd02bc0f8
Quoc141119 <cubi12c6@gmail.com>
2019-06-12 17:13:51 +0700

    + add function MyPoint(), MyPoint(int,int), ~MyPoint()

---
+++ OPP_MyPoint/OPP_MyPoint/MyPoint.cpp
@@ -0,0 +1,19 @@
+#include "MyPoint.h"
+#include <iostream>
+#include <math.h>
+
+
+MyPoint::MyPoint(int x, int y)
+{
+	this->SetX(x);
+	this->SetY(y);
+}
+
+MyPoint::MyPoint()
+{
+}
+
+
+MyPoint::~MyPoint()
+{
+}
