4052e31df24d1c9f1661390d526f3d8b692148fc
Quoc141119 <cubi12c6@gmail.com>
2019-06-17 11:46:07 +0700

    + finish all function

--- MyVirus/MyVirus/Virus.h
+++ MyVirus/MyVirus/Virus.h
@@ -1,18 +1,26 @@
 #pragma once
+#include <list>
+#include <fstream>
+#include <iostream>
+using namespace std;
+
+
 class Virus
 {
 private:
-	char* m_dna;
+	char *m_dna;
 	int m_resistance;
 public:
 	Virus();
-	Virus(Virus *vr);
+	Virus(const Virus *vr);
+	void Setresistance(int resistance);
+	int Getresistance();
 	void LoadADNInformation();
-	void ReduceResistance();
+	int ReduceResistance(int medicine_esistance);
 	~Virus();
-	virtual void DoBorn();
-	virtual void DoClone();
-	virtual void DoDie();
-	virtual void InitResistance();
+	virtual void DoBorn() {};
+	virtual list<Virus*> DoClone() = 0;
+	virtual void DoDie() {};
+	virtual void InitResistance() {};
 };
 
