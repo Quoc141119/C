f7a98b9609bf4025a06f72344a1deabf262bc26a
Quoc141119 <cubi12c6@gmail.com>
2019-06-17 11:45:43 +0700

    + finish all function

--- MyVirus/MyVirus/Virus.cpp
+++ MyVirus/MyVirus/Virus.cpp
@@ -1,54 +1,51 @@
 #include "Virus.h"
-
-
+using namespace std;
 
 Virus::Virus()
 {
 }
 
-Virus::Virus(Virus *vr)
+void Virus::Setresistance(int resistance)
 {
-	this->m_dna = vr->m_dna;
-	this->m_resistance = vr->m_resistance;
+	m_resistance = resistance;
+}
+
+int Virus::Getresistance()
+{
+	return m_resistance;
+}
+
+Virus::Virus(const Virus *vr)
+{
+	m_dna = vr->m_dna;
+	m_resistance = vr->m_resistance;
 }
 
 void Virus::LoadADNInformation()
 {
-
+	ifstream fileinput;
+	fileinput.open("ATGX.bin");
+	if (fileinput.is_open())
+	{
+		char a[100];
+		fileinput >> a;
+		m_dna = (char *) a;
+	}
+	else
+	{
+		cout << "file isn't open";
+	}
+	fileinput.close();
 }
 
-void Virus::ReduceResistance()
+int Virus::ReduceResistance(int medicien_resistance)
 {
-
+	this->m_resistance = this->m_resistance - medicien_resistance;
+	return this->m_resistance;
 }
 
 Virus::~Virus()
 {
-	if (m_dna == nullptr)
-	{
-		delete[]m_dna;
-	}
 }
-
-void Virus::DoBorn()
-{
-
-}
-
-void Virus::DoClone()
-{
-
-}
-
-void Virus::DoDie()
-{
-
-}
-
-void Virus::InitResistance()
-{
-
-}
-
 
 
